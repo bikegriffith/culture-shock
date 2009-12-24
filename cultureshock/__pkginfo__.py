b24a6c4cba7a61fe5e5ca2da40425f5ea27b8aa0
mgriffith <mgriffith@mgriffith-pc.Belkin>
2009-12-24 13:16:34 -0500

    added package info, paver script, and basic package layout.

---
+++ cultureshock/__pkginfo__.py
@@ -0,0 +1,34 @@
+"""culture-shock packaging information"""
+# pylint: disable-msg=C0103,W0622
+
+libname = 'culture-shock'
+modname = 'cultureshock'
+packages = ['cultureshock']
+
+version = '0.0.1dev'
+
+license = 'MIT'
+copyright = 'Copyright (c) 2009 Mike Griffith (bikegriffith@gmail.com)'
+
+short_desc = 'A tool for analyzing code quality of team members.'
+long_desc = """\
+ Culture-shock provides a framework for turning programming into a game."""
+
+author = 'Mike Griffith'
+author_email = 'bikegriffith@gmail.com'
+
+url = 'http://code.google.com/p/culture-shock'
+download_url = 'http://code.google.com/p/culture-shock/downloads/list'
+mailing_list = 'http://groups.google.com/group/culture-shock'
+
+# For list of all classifiers, see:
+# http://pypi.python.org/pypi?:action=list_classifiers
+classifiers =  [
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Software Development :: Quality Assurance',
+]
